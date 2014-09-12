c93aaf975afc48c59e54413530c214d3a6a2037b
dikshie <dikshie@gmail.com>
2014-09-13 01:34:37 +0900

    add explanation

--- installmonitoringtools.sh
+++ installmonitoringtools.sh
@@ -2,5 +2,5 @@
 
 yum -y install htop   #we can see per processor utilization
 yum -y install iptraf-ng #we can see ip traffic on every interfaces
-yum -y install monit #monit is realtime monitoring, that also can be viewed for its webserver
+yum -y install monit #monit is near realtime monitoring (process, load, applications, etc), that also can be viewed for its webserver. for near realtime monitoring we can decrease the checking interval to 5 seconds. 
  
